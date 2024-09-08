9efbe4facb084b3b2558a36f0e302df026da138a
Valera Bitkovsky <bitvalser@gmail.com>
2024-09-08 06:47:25 +0300

    minor fix

--- server/src/core/ensure-role.ts
+++ server/src/core/ensure-role.ts
@@ -3,7 +3,7 @@ import { NextFunction, Request, Response } from 'express';
 export const ensureRole =
   (role: string[] = null): any =>
   async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-    if (Array.isArray(role) && role.includes(req.userRole)) {
+    if (Array.isArray(role) && !role.includes(req.userRole)) {
       res.sendStatus(403);
     } else {
       next();
